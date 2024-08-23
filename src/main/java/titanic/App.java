3b7016940333c12ac4dd4c47410a5145b1262420
dileep2705 <dileepkumar2705@gmail.com>
2024-08-23 12:20:44 +0530

    Add files via upload

---
+++ src/main/java/titanic/App.java
@@ -0,0 +1,13 @@
+package titanic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
